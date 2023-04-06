d6362576505fce24423cbd5b02f83c009867d501
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2023-04-06 14:52:02 +0700

    rewrite all using composite template
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,86 +1,24 @@
+mod window;
+
 use gtk::{
+    gio::resources_register_include,
     glib,
-    glib::clone,
     prelude::{ApplicationExt, ApplicationExtManual},
-    traits::{BoxExt, ButtonExt, GtkWindowExt, WidgetExt},
-    Application, ApplicationWindow, Button, DropDown, Switch,
+    traits::GtkWindowExt,
+    Application,
 };
-use std::{cell::Cell, rc::Rc};
+use window::Window;
 
 const APP_ID: &str = "io.github.aerphanas";
 
-fn main() {
-    let app: Application = Application::builder().application_id(APP_ID).build();
+fn main() -> glib::ExitCode {
+    resources_register_include!("src_gresource.resources").expect("Failed to register resources");
+    let app = Application::builder().application_id(APP_ID).build();
     app.connect_activate(build_ui);
-    app.run();
+    app.run()
 }
 
 fn build_ui(app: &Application) {
-    let operator_choose: DropDown = DropDown::from_strings(&["AND", "OR", "XOR"]);
-    let button_ressult: Button = Button::builder().label("Callculate").build();
-    let left_val: Rc<Cell<bool>> = Rc::new(Cell::new(false));
-    let right_val: Rc<Cell<bool>> = Rc::new(Cell::new(false));
-
-    let gtk_box: Vec<gtk::Box> = (0..1)
-        .map(|_| {
-            let switch: Vec<Switch> = (0..3)
-                .map(|num| {
-                    let switch_child = Switch::new();
-                    switch_child.connect_state_notify(clone!(@weak right_val, @weak left_val =>
-                    move |switch_child| {
-                        match num {
-                            0 => left_val.set(switch_child.state()),
-                            1 => right_val.set(switch_child.state()),
-                            _ => ()
-                        }
-                    }));
-                    switch_child
-                })
-                .collect();
-
-            button_ressult.connect_clicked(
-                clone!(@strong right_val, @strong left_val, @strong switch, @weak operator_choose =>
-                    move |_| {
-                        let result: bool = match operator_choose.selected() {
-                            0 => right_val.get() && left_val.get(),
-                            1 => right_val.get() || left_val.get(),
-                            2 => right_val.get() ^ left_val.get(),
-                            _ => false
-                        };
-                        switch
-                            .last()
-                            .unwrap()
-                            .set_active(result)
-                    }
-                ),
-            );
-            let boxs: gtk::Box = gtk::Box::builder()
-                .orientation(gtk::Orientation::Horizontal)
-                .build();
-            switch.last().unwrap().set_sensitive(false);
-            switch.iter().for_each(|switch| boxs.append(switch));
-            boxs
-        })
-        .collect();
-
-    let box_parent: gtk::Box = gtk::Box::builder()
-        .orientation(gtk::Orientation::Vertical)
-        .margin_start(10)
-        .margin_end(10)
-        .margin_top(10)
-        .margin_bottom(10)
-        .build();
-
-    box_parent.append(&operator_choose);
-    box_parent.append(&button_ressult);
-    gtk_box.iter().for_each(|boxs| box_parent.append(boxs));
-
-    let window: ApplicationWindow = ApplicationWindow::builder()
-        .application(app)
-        .title("Bits")
-        .default_width(160)
-        .child(&box_parent)
-        .build();
-
+    let window = Window::new(app);
     window.present();
 }
---
+++ src/window/imp.rs
@@ -0,0 +1,95 @@
+use std::{cell::Cell, rc::Rc};
+
+use gtk::{
+    glib::{self, clone, subclass::InitializingObject},
+    subclass::{
+        prelude::{ApplicationWindowImpl, ObjectImpl, ObjectImplExt, ObjectSubclass},
+        widget::{
+            CompositeTemplateClass, CompositeTemplateInitializingExt, WidgetClassSubclassExt,
+            WidgetImpl,
+        },
+        window::WindowImpl,
+    },
+    traits::ButtonExt,
+    Button, CompositeTemplate, DropDown, Switch, TemplateChild,
+};
+
+#[derive(Default, CompositeTemplate)]
+#[template(resource = "/io/github/aerphanas/window.ui")]
+pub struct Window {
+    #[template_child]
+    pub drop_down_option: TemplateChild<DropDown>,
+
+    #[template_child]
+    pub calculate_button: TemplateChild<Button>,
+
+    #[template_child]
+    pub left_switch: TemplateChild<Switch>,
+
+    #[template_child]
+    pub right_switch: TemplateChild<Switch>,
+
+    #[template_child]
+    pub result_switch: TemplateChild<Switch>,
+}
+
+#[glib::object_subclass]
+impl ObjectSubclass for Window {
+    const NAME: &'static str = "BitsAppWindow";
+    type Type = super::Window;
+    type ParentType = gtk::ApplicationWindow;
+
+    fn class_init(klass: &mut Self::Class) {
+        klass.bind_template();
+    }
+
+    fn instance_init(obj: &InitializingObject<Self>) {
+        obj.init_template();
+    }
+}
+
+impl ObjectImpl for Window {
+    fn constructed(&self) {
+        self.parent_constructed();
+
+        let left_val = Rc::new(Cell::new(false));
+        let right_val = Rc::new(Cell::new(false));
+        let operator_choose = self.drop_down_option.get();
+        let ress_switch = self.result_switch.get();
+
+        self.calculate_button.connect_clicked(clone!(
+            @strong right_val,
+            @strong left_val,
+            @strong operator_choose,
+        @strong ress_switch,
+            @weak self as window => move |_| {
+                let result = match operator_choose.selected() {
+                    0 => right_val.get() && left_val.get(),
+                    1 => right_val.get() || left_val.get(),
+                    2 => right_val.get() ^ left_val.get(),
+                    _ => false
+                };
+                ress_switch.set_active(result);
+                println!("{}", operator_choose.selected());
+                println!("{}", left_val.get());
+                println!("{}", right_val.get());
+            }
+        ));
+
+        self.left_switch
+            .connect_state_notify(clone!(@weak left_val =>
+            move |switch_child| {
+                    left_val.set(switch_child.state());
+            }));
+
+        self.right_switch
+            .connect_state_notify(clone!(@weak right_val =>
+            move |switch_child| {
+                    right_val.set(switch_child.state());
+            }));
+    }
+}
+
+impl WidgetImpl for Window {}
+impl WindowImpl for Window {}
+impl ApplicationWindowImpl for Window {}
